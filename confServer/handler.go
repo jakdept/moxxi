c9f789c67cb7e3d4691763c76dbc674fd56ecf5f
Jack Hayhurst <James.A.Hayhurst@gmail.com>
2016-03-13 01:33:26 -0500

    fixed a bunch of stuff in QueryHandler

--- confServer/handler.go
+++ confServer/handler.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"text/template"
+	"strconv"
 )
 
 func QueryHandler(baseURL, confPath, confExt, mainDomain string,
@@ -14,44 +15,43 @@ func QueryHandler(baseURL, confPath, confExt, mainDomain string,
 	return func(w http.ResponseWriter, r *http.Request) {
 		values := r.URL.Query()
 		var tls bool
-		var url string
+		var err error
 
 		if len(values["host"]) < 1 {
-			http.Error(w, errors.New("no provided hostname"), http.StatusPreconditionFailed)
+			http.Error(w, "no provided hostname", http.StatusPreconditionFailed)
 			// TODO some log line?
 			return
 		}
 
 		if len(values["ip"]) < 1 {
-			http.Error(w, errors.New("no provided ip"), http.StatusPreconditionFailed)
+			http.Error(w, "no provided ip", http.StatusPreconditionFailed)
 			// TODO some log line?
 			return
 		}
 
-		if tls, err := strconf.ParseBool(values["tls"][0]); err != nil {
+		if tls, err = strconv.ParseBool(values["tls"][0]); err != nil {
 			tls = DefaultBackendTLS
 		}
 
 		config, err := confCheck(values["host"][0], values["ip"][0], tls,
 			values["blockedHeaders"])
 		if err != nil {
-			http.Error(w, err, http.StatusPreconditionFailed)
+			http.Error(w, err.Error(), http.StatusPreconditionFailed)
 			// TODO some log line?
 			return
 		}
 
 		if config.ExtHost, err = confWriter(config); err != nil {
-			http.Error(w, err, http.InternalServerError)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			// TODO some log line? or no?
 			return
 		}
 
 		if err = resTempl.Execute(w, config); err != nil {
-			http.Error(w, err, http.InternalServerError)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			// TODO some long line? or no?
 			return
 		}
-
 		return
 	}
 }
@@ -61,6 +61,6 @@ func FormHandler(baseURL, confPath, confExt string, subdomainLength int,
 	done <-chan struct{}) http.HandlerFunc {
 
 	return func(w http.ResponseWriter, r *http.Request) {
-		r.Parse
+		// r.ParseForm
 	}
 }
