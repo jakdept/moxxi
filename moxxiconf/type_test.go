fd61a6d4954fdad411096cba727296081e585ea9
Jack Hayhurst <James.A.Hayhurst@gmail.com>
2016-03-14 03:49:08 -0400

    added a test for HandlerLocFlag

--- moxxiconf/type_test.go
+++ moxxiconf/type_test.go
@@ -35,7 +35,29 @@ func Err_Error_test(t *testing.T) {
 			"was not given a new random domain - shutting down",
 		},
 	}
-	for _, test := range testData{
+	for _, test := range testData {
 		assert.Equal(t, test.out, test.in.Error(), "errors should match")
 	}
+}
+
+func HandlerLocFlag_test(t *testing.T) {
+	var testData = []string{
+		"one",
+		"two",
+		"three",
+		"four",
+	}
+
+	var testWork HandlerLocFlag
+
+	for _, each := range testData {
+		assert.NoError(t, testWork.Set(each), "there should not have been a problem adding an item")
+	}
+
+	assert.Equal(t, testData, []string(testWork), "the test input and current value of the test should be equal")
+
+	for i := 0; i < len(testData); i++ {
+		assert.Equal(t, testData[i], testWork.GetOne(i), "the test input and current value of the test should be equal")
+	}
+
 }
