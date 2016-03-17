7c93da8b9a2fd28c8219367b045e814aa7d3c69c
Jack Hayhurst <James.A.Hayhurst@gmail.com>
2016-03-17 00:05:35 -0400

    started writing the handler_test stuff

--- moxxiconf/handler_test.go
+++ moxxiconf/handler_test.go
@@ -1,1 +1,36 @@
 package moxxiConf
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestFormHandler(t *testing.T) {
+	var testData = []struct {
+		reqMethod string
+		reqURL    string
+		reqBody   string
+		resBody   string
+		resCode   int
+		fileOut   string
+	}{
+		{
+			reqMethod: "POST",
+			reqURL:    "domain.com",
+			reqBody:   ``,
+			resBody:   ``,
+			resCode:   200,
+			fileOut:   ``,
+		},
+	}
+
+	templPath := os.TempDir()
+	templExt := ".out"
+	baseURL := "domain.com"
+	subdomainLen := 1
+	excludes := []string{"a.domain.com", "b.domain.com", "c.domain.com"}
+	templString := `{{.IntHost}} {{.IntIP}} {{.IntPort}} {{.Encrypted}} {{ range .StripHeaders }}{{.}} {{end}}`
+	templ := template.Must(template.New("testing").Parse(templateString))
+
+}
