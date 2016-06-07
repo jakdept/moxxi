e0e803209aef834d16604cd2084e3044d8db49fa
James Hayhurst <James.A.Hayhurst@gmail.com>
2016-06-06 22:04:01 -0400

    started reworking config stuff, abandoning viper as it is not working

--- moxxiconf/config.go
+++ moxxiconf/config.go
@@ -1,22 +1,24 @@
 package moxxiConf
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/spf13/viper"
 	"log"
+	"io/ioutil"
+	"os"
 	"text/template"
 )
 
-func LoadConfig() ([]string, []HandlerConfig, Err) {
-	config := prepConfig()
 
-	if err := config.ReadInConfig(); err != nil {
-		log.Printf("Fatal error config file: %s \n", err)
-		return []string{}, []HandlerConfig{}, UpgradeError(err)
+func LoadConfig() ([]string, []HandlerConfig, Err) {
+	config, err := prepConfig()
+	if err != nil {
+		return []string{}, []HandlerConfig{}, err
 	}
 
-	if err := verifyConfig(config); err != nil {
-		return []string{}, []HandlerConfig{}, UpgradeError(err)
+	if err = validateConfig(config); err != nil {
+		return []string{}, []HandlerConfig{}, err
 	}
 
 	handlers, listens, err := loadConfig(config)
@@ -27,25 +29,106 @@ func LoadConfig() ([]string, []HandlerConfig, Err) {
 	return handlers, listens, nil
 }
 
-func prepConfig() *viper.Viper {
 
-	c := viper.New()
-	// establish the config paths
-	c.SetConfigName("config")
-	c.AddConfigPath("/etc/moxxi/")
-	c.AddConfigPath("$HOME/.moxxi")
-	c.AddConfigPath(".")
+func prepConfig() (*map[string]interface{}, Err) {
 
-	// set default values for the config
-	c.SetDefault("listen", []string{":8080"})
+	possibleConfigs := []string{
+		"./config.json",
+		"/etc/moxxi/config.json",
+		"$HOME/.moxxi/config.json",
+		"./moxxi.config.json",
+	}
+
+	var data []byte
+	var globErr error
+	var c *map[string]interface{}
+	for _, configTry := range possibleConfigs {
+		data, globErr := ioutil.ReadFile(configTry)
+		switch globErr {
+		case os.ErrNotExist:
+			continue
+		case nil:
+			break
+		default:
+			return nil, NewErr{
+				Code: ErrConfigBadRead,
+				fmt.Errorf("bad config file - %s", configTry),
+			}
+		}
+	}
+
+	globErr := json.Unmarshal(data, c)
+	if globErr != nil {
+		return nil, UpgradeError(globErr)
+	}
+
+	if _, ok := c["listen"]; !ok {
+		c["listen"] = []string{"localhost:8080"}
+	}
 
 	return c
+}
 
+func validateConfig(c *map[string]interface{}) Err {
+	// clean up array top level lines
+	for _, part := range []string{"listen", "exclude"} {
+		if _, ok := c[part]; ok {
+			if _, ok := c[part].([]interface{}); !ok {
+				c[part] = []interface{}{c[part]}
+			}
+
+			if _, ok := c[part].([]string); !ok {
+				return NewErr{Code: ErrConfigBadStructure, value: part}
+			}
+		}
+	}
+
+	// check that the following things are strings in the top level
+	for _, part := range []string{
+		"baseURL",
+		"confFile",
+		"confExt",
+		"confFile",
+		"resFile",
+	} {
+		if _, ok := c[part]; ok {
+			if _, ok := c[part].(string); !ok {
+				return NewErr{Code: ErrConfigBadStructure, value: part}
+			}
+		}
+	}
+
+	if _, ok := c["subdomainLen"]; ok {
+		var subdomainLen int
+		if subdomainLen, ok := c["subdomainLen"].(int); !ok {
+			return NewErr{Code: ErrConfigBadStructure, value: "subdomainLen"}
+		}
+		if subdomainLen < 8 {
+			c["subdomainLen"] = 8
+		}
+	}
+
+	// verify the handlers are an array
+	handlers, ok := c["handler"].([]interface{})
+	if !ok {
+		return NewErr{
+			Code:  ErrConfigBadStructure,
+			value: "handler",
+		}
+	}
+
+	// test and propagate handlers
+	for id, eachHandler := range handlers {
+		locErr := validateConfigHandler(c, id, eachHandler)
+		if locErr != nil {
+			return locErr
+		}
+	}
+
+	return nil
 }
 
 func loadConfig(c *viper.Viper) ([]string, []HandlerConfig, Err) {
-
-	viper.Debug()
 
 	var handlers []HandlerConfig
 	var listens []string
@@ -63,139 +146,4 @@ func loadConfig(c *viper.Viper) ([]string, []HandlerConfig, Err) {
 	}
 
 	return listens, handlers, nil
-}
-
-func verifyConfig(c *viper.Viper) Err {
-
-	// clean up the listen line
-	switch c.Get("listen").(type) {
-	case []string:
-	case string:
-		c.Set("listen", []string{c.GetString("listen")})
-	default:
-		return NewErr{
-			Code:  ErrConfigBadStructure,
-			value: "listen",
-		}
-	}
-
-	// verify the handlers are an array
-	handlers, ok := c.Get("handler").([]interface{})
-	if !ok {
-		return NewErr{
-			Code:  ErrConfigBadStructure,
-			value: "handler",
-		}
-	}
-
-	// check that the following things are strings in the top level
-	for _, subConf := range []string{
-		"baseURL",
-		"confFile",
-		"confExt",
-		"confFile",
-		"resFile",
-	} {
-		if c.IsSet(subConf) {
-			if _, ok := c.Get(subConf).(string); !ok {
-				return NewErr{Code: ErrConfigBadStructure, value: subConf}
-			}
-		}
-	}
-
-	// toplevel other typechecks
-	if c.IsSet("excludes") {
-		if _, ok := c.Get("excludes").([]string); !ok {
-			return NewErr{Code: ErrConfigBadStructure, value: "excludes"}
-		}
-	}
-	if c.IsSet("subDomainLen") {
-		if _, ok := c.Get("subDomainLen").(int); !ok {
-			return NewErr{Code: ErrConfigBadStructure, value: "subDomainLen"}
-		}
-	}
-
-	// verify similar for individual handlers
-	for i := 0; i < len(handlers); i++ {
-		base := fmt.Sprintf("handler.%d.", i)
-
-		if !c.IsSet(base + "handlerType") {
-			c.Set(base+".handlerType", "static")
-		}
-
-		if !c.IsSet(base + "handlerRoute") {
-			return NewErr{
-				Code:  ErrConfigBadValue,
-				value: "handlerRoute",
-			}
-		}
-
-		// string typecheck
-		for _, subConf := range []string{
-			"baseURL",
-			"confFile",
-			"confExt",
-			"confFile",
-			"resFile",
-		} {
-			if c.IsSet(base + subConf) {
-				if _, ok := c.Get(base + subConf).(string); !ok {
-					return NewErr{Code: ErrConfigBadStructure, value: base + subConf}
-				}
-			}
-		}
-
-		// other typechecks
-		if c.IsSet(base + "excludes") {
-			if _, ok := c.Get(base + "excludes").([]string); !ok {
-				return NewErr{Code: ErrConfigBadStructure, value: base + "excludes"}
-			}
-		}
-		if c.IsSet(base + "subdomainLen") {
-			if _, ok := c.Get(base + "subdomainLen").([]string); !ok {
-				return NewErr{Code: ErrConfigBadStructure, value: base + "subdomainLen"}
-			}
-		}
-
-		// propagate unset values from the global setting
-		for _, subConf := range []string{
-			"baseURL",
-			"confFile",
-			"confExt",
-			"confFile",
-			"resFile",
-			"excludes",
-			"subdomainLen",
-		} {
-			if !c.IsSet(base+subConf) && c.IsSet(subConf) {
-				c.Set(base+subConf, c.Get(subConf))
-			}
-		}
-
-		// parse the two templates
-		if c.GetString(base+"handlerType") != "static" && c.IsSet(base+"confFile") {
-			template, err := template.ParseFiles(c.GetString(base + "confFile"))
-			if err != nil {
-				return NewErr{
-					Code:    ErrConfigBadTemplate,
-					value:   c.GetString(base + "handlerType"),
-					deepErr: err,
-				}
-			}
-			c.Set(base+"confTempl", template)
-		}
-
-		if c.GetString(base+"handlerType") != "static" && c.IsSet(base+"resFile") {
-			template, err := template.ParseFiles(c.GetString(base + "resFile"))
-			if err != nil {
-				return NewErr{
-					Code:    ErrConfigBadTemplate,
-					value:   c.GetString(base + "handlerType"),
-					deepErr: err,
-				}
-			}
-			c.Set(base+"resTempl", template)
-		}
-	}
-	return nil
 }
