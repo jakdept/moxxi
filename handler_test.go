176e86eed9c8d6f6c0c0c406fc0b634556371edd
Jack Hayhurst <jakdept@gmail.com>
2017-07-01 16:52:27 -0400

    added a test for the replacer

--- handler_test.go
+++ handler_test.go
@@ -44,27 +44,33 @@ func TestReplacerVanilla(t *testing.T) {
 	goldie.Assert(t, t.Name(), buf.Bytes())
 }
 
-// func TestReplacerReversed(t *testing.T) {
-// 	t.Parallel()
-// 	defer func() {
-// 		err, ok := recover().(error)
-// 		if !assert.False(t, ok) {
-// 			t.FailNow()
-// 		}
-// 		if !assert.NoError(t, err) {
-// 			t.FailNow()
-// 		}
-// 	}()
+func TestReplacerReversed(t *testing.T) {
+	t.Parallel()
+	defer func() {
+		errInt := recover()
+		if errInt != nil {
+			err, ok := errInt.(error)
+			if !assert.True(t, ok) {
+				t.FailNow()
+			}
+			if !assert.NoError(t, err) {
+				t.FailNow()
+			}
+		}
+	}()
 
-// 	file, err := os.Open("testdata/lorem_ipsum.input")
-// 	if !assert.NoError(t, err) {
-// 		t.FailNow()
-// 	}
-// 	defer file.Close()
-// 	r := Replacer{old: []byte("123"), new: []byte("cum")}
-// 	r.Reverse()
-// 	data := []byte{}
-// 	buf := bytes.NewBuffer(data)
-// 	r.replace(file, buf)
-// 	goldie.Assert(t, t.Name(), data)
-// }
+	file, err := os.Open("testdata/technologic.txt")
+	if !assert.NoError(t, err) {
+		t.FailNow()
+	}
+	defer file.Close()
+	r := Replacer{
+		old:      []byte(" that"),
+		new:      []byte(" it"),
+		memUsage: 50,
+	}
+	buf := &bytes.Buffer{}
+	r.Reverse()
+	r.replace(file, buf)
+	goldie.Assert(t, t.Name(), buf.Bytes())
+}
