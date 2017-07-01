4b6aa1281ab4bbfa192c49111bb28b325228c140
Jack Hayhurst <jakdept@gmail.com>
2017-07-01 17:03:42 -0400

    got all relevant errors in replacment working

--- handler_test.go
+++ handler_test.go
@@ -1,6 +1,7 @@
 package moxxi
 
 import (
+	"errors"
 	"os"
 	"testing"
 
@@ -43,6 +44,33 @@ func TestReplacerVanilla(t *testing.T) {
 	r.replace(file, buf)
 	goldie.Assert(t, t.Name(), buf.Bytes())
 }
+func TestReplacerSmallBuffer(t *testing.T) {
+	t.Parallel()
+	defer func() {
+		if err, ok := recover().(error); !ok {
+			t.Error("recover did not return an error")
+			t.FailNow()
+		} else {
+			if !assert.Equal(t, err,
+				errors.New("not enough memory allocated for memory slice")) {
+				t.FailNow()
+			}
+		}
+	}()
+
+	file, err := os.Open("testdata/technologic.txt")
+	if !assert.NoError(t, err) {
+		t.FailNow()
+	}
+	defer file.Close()
+	r := Replacer{
+		old:      []byte("really long replacement string"),
+		new:      []byte("junk really"),
+		memUsage: 1, // far too short for the buffer size
+	}
+	buf := &bytes.Buffer{}
+	r.replace(file, buf)
+}
 
 func TestReplacerReversed(t *testing.T) {
 	t.Parallel()
@@ -67,7 +95,7 @@ func TestReplacerReversed(t *testing.T) {
 	r := Replacer{
 		old:      []byte(" that"),
 		new:      []byte(" it"),
-		memUsage: 50,
+		memUsage: -1,
 	}
 	buf := &bytes.Buffer{}
 	r.Reverse()
